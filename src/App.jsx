1b45419375fac879757e4de0a3ce5594af3542f9
sametkzdl <sametkazdal_53@outlook.com>
2025-04-17 22:14:18 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="xAkademicAMPA-GN">
+    <BrowserRouter basename="/xAkademicAMPA-GN">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
